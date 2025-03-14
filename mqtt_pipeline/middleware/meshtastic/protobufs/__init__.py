94dc8078900194ad5568499ee78ee64d144f0464
nisfeb <business@nisfeb.com>
2025-03-13 21:25:42 -0400

    Expose internal behavior

--- mqtt_pipeline/middleware/meshtastic/protobufs/__init__.py
+++ mqtt_pipeline/middleware/meshtastic/protobufs/__init__.py
@@ -1,1 +1,7 @@
-from .meshtastic import *
+# from .meshtastic import *
+from .meshtastic import config_pb2
+from .meshtastic import mesh_pb2
+from .meshtastic import mqtt_pb2
+
+# Export them all
+__all__ = ['config_pb2', 'mesh_pb2', 'mqtt_pb2']
